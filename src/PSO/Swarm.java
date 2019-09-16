e8ce73aeddc3fcf8db36be92ea7322a06235d6dc
Kamini Prakash <46829105+KaminiPremPrakash@users.noreply.github.com>
2019-09-16 11:46:55 -0400

    Adding to Public repo

---
+++ src/PSO/Swarm.java
@@ -0,0 +1,249 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PSO;
+
+import Business.Executor;
+import Business.Graph;
+import Business.LocationModel;
+
+import Business.ParticleModel;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author sojit
+ */
+public class Swarm {
+
+    public ArrayList<Double> gBestRoute; // pbest
+    double gFitnessValue;
+    double[] gBestVelocity;
+    Thread process;
+    Executor exe;
+    double[] value = new double[3];
+    double pprev=0;
+    double prev=0;
+    double curr=0;
+    private final ParticleModel parModel;
+    public Graph graph;
+    public LocationModel loc;
+
+    public Swarm(ParticleModel db, LocationModel loc, int locationCount, Graph graph) {
+        //find global best	
+        this.parModel = db;
+        this.graph = graph;
+        this.loc = loc;
+        gBestRoute = new ArrayList();
+        gBestVelocity = new double[locationCount];
+        gFitnessValue = Double.MAX_VALUE;
+        findGlobalBest(parModel.getArraySalesperson());
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void findGlobalBest(Particle[] arrPar) {
+        for (Particle par : arrPar) {
+            if (par.pFitnessValue < gFitnessValue) {
+                gFitnessValue = par.pFitnessValue;
+                gBestRoute = par.particleRoute;
+                gBestVelocity = par.pBestVelocity;
+               
+            }
+        }
+
+        System.out.println("---------------Global Solution---------------");
+        System.out.println("global FitnessValue  " + gFitnessValue);
+        System.out.println("global BestRoute " + gBestRoute);
+        System.out.println("global BestVelocity" + Arrays.toString(gBestVelocity));
+        // throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        System.out.println("\n");
+
+    }
+
+    public boolean calculatebestSolution(int target) {
+
+        for (Particle p : parModel.getArraySalesperson()) {
+            exe = new Executor(p, graph, gBestRoute);
+            process = new Thread(exe);
+            process.start();
+
+        }
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException ex) {
+            Logger.getLogger(Swarm.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        //update all global variables after each paricle updated
+        findGlobalBest(parModel.getArraySalesperson());
+        pprev = prev;
+               prev=curr;
+                curr = gFitnessValue;
+        try {
+            Thread.sleep(2000);
+
+            //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        } catch (InterruptedException ex) {
+            Logger.getLogger(Swarm.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        if (gFitnessValue <= target) {
+            return true;
+        } else if(curr == pprev){
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public int[] getBestRoute() {
+        int bestRoute[] = new int[gBestVelocity.length];
+        Map<Double, List<Integer>> frequency = new HashMap<>();
+
+        for (int i = 0; i < gBestRoute.size(); i++) {
+            if (frequency.get(gBestRoute.get(i)) == null) {
+                frequency.put(gBestRoute.get(i), new ArrayList<Integer>());
+            }
+            frequency.get(gBestRoute.get(i)).add(i);
+        }
+
+        Collections.sort(gBestRoute);
+
+        for (int i = 0; i < bestRoute.length; i++) {
+            if (frequency.get(gBestRoute.get(i)).size() > 1) {
+                int num = i;
+                for (int k = 0; k < frequency.get(gBestRoute.get(num)).size(); k++) {
+                    bestRoute[i] = frequency.get(gBestRoute.get(num)).get(k) + 1;
+                    i++;
+                }
+
+            } else {
+                bestRoute[i] = frequency.get(gBestRoute.get(i)).get(0) + 1;
+            }
+        }
+        return bestRoute;
+    }
+
+    public void trackResultOfParticle(int num, Map<Double, Map<Double, Double>> particles) {
+        double count = 1;
+        for (Particle p : parModel.getArraySalesperson()) {
+            if (particles.get(count) == null) {
+                particles.put(count, new HashMap<>());
+            }
+            particles.get(count).put((double) num, p.pBestValue);
+            count++;
+        }
+    }
+
+    public Map<String, List<Integer>> CountBestRouteRounds(int[] bestRoute) {
+        Map<String, List<Integer>> model = new HashMap<>();
+        List<Integer> route;
+        int roundsCount = 0;
+        int totalDistance = 0;
+
+        for (int v = 0; v < parModel.getArraySalesperson().length; v++) {
+            route = new ArrayList<>();
+            route.add(0);
+            roundsCount = 0;
+
+            int availableCapacity = parModel.getArraySalesperson()[v].maxCapacity;
+            for (int i = 0; i < bestRoute.length; i++) {
+
+                int locDemand = loc.Locations[bestRoute[i] - 1].maxDemand;
+
+                if (availableCapacity == locDemand) {
+                    route.add(bestRoute[i]);
+                    if(route.get(route.size()-1) != 0)
+                    route.add(0);
+                    roundsCount++;
+
+                    availableCapacity = parModel.getArraySalesperson()[v].maxCapacity;
+                    locDemand = 0;
+
+                } else {
+                    while (locDemand > 0) {
+
+                        if (locDemand > availableCapacity) {
+                            locDemand = locDemand - availableCapacity;
+                            route.add(bestRoute[i]);
+                            if(route.get(route.size()-1) != 0)
+                            route.add(0);
+                            roundsCount++;
+                            availableCapacity = parModel.getArraySalesperson()[v].maxCapacity;
+                            if (availableCapacity == locDemand) {
+
+                                if(route.get(route.size()-1) != 0)
+                                route.add(0);
+                                roundsCount++;
+                                availableCapacity = parModel.getArraySalesperson()[v].maxCapacity;
+
+                            }
+
+                        } else if (locDemand < availableCapacity) {
+                            availableCapacity = availableCapacity - locDemand;
+                            route.add(bestRoute[i]);
+                            locDemand = 0;
+                        } else {
+                            route.add(bestRoute[i]);
+                            locDemand = 0;
+                            if(route.get(route.size()-1) != 0)
+                            route.add(0);
+                            roundsCount++;
+
+                            availableCapacity = parModel.getArraySalesperson()[v].maxCapacity;
+                           // System.out.println("finaly" + availableCapacity + " " + locDemand);
+                        }
+                    }
+
+                }
+            }
+
+            model.put("Salesperson " + parModel.getArraySalesperson()[v].name + " Capacity : " + parModel.getArraySalesperson()[v].maxCapacity + ", Total Rounds : " + roundsCount + "  , Best Route", route);
+           
+        }
+
+//                while (locDemand > 0) {
+//                    roundsCount++;
+//                   
+//                    if(availableCapacity > locDemand )
+//                    {
+//                        int a = availableCapacity -locDemand;
+//                        locDemand = locDemand-a;
+//                        availableCapacity = availableCapacity-a;
+//                        if(availableCapacity <= 0)
+//                        {
+//                              route.add(0);
+//                              availableCapacity = parModel.getArraySalesperson()[v].maxCapacity;
+//                        }
+//                    }
+//                    else
+//                    {
+//                        int a = locDemand-availableCapacity;
+//                        locDemand = locDemand-a;
+//                        availableCapacity = locDemand-a;
+//                         if(availableCapacity <= 0)
+//                        {
+//                              route.add(0);
+//                              availableCapacity = parModel.getArraySalesperson()[v].maxCapacity;
+//                        }
+//                       
+//                    }
+//                   
+//                    //locDemand = locDemand - availableCapacity;
+//                    
+//                    route.add(bestRoute[i]);
+//                }
+        return model;
+    }
+
+}
